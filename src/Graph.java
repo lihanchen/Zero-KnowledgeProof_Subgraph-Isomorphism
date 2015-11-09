ff2397e66248646a823ae53e88e3c1c7b0c03690
Hanchen Li <lihanchen@users.noreply.github.com>
2015-11-08 19:07:29 -0500

    Graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,67 +1,107 @@
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class Graph {
-	int size;
-	boolean edge[][];
-	Scanner scanner;
+	HashSet<Integer> vertex;
+	HashSet<Pair<Integer, Integer>> edges;
 
-	public Graph(Scanner scanner) {
-		this.scanner = scanner;
+
+	public Graph() {
 		LinkedList<Boolean> ll = new LinkedList<Boolean>();
 		int i = 0;
 		try {
 			while (true) {
-				ll.add(scanner.nextInt() == 1);
+				ll.add(Proof.scanner.nextInt() == 1);
 				i++;
 			}
 		} catch (NoSuchElementException e) {
-			scanner.nextLine();
 		}
 
-		size = (int) Math.sqrt(i);
-		edge = new boolean[size][size];
+		vertex = new HashSet<Integer>();
+		edges = new HashSet<Pair<Integer, Integer>>();
+
+		int size = (int) Math.sqrt(i);
+		for (i = 1; i <= size; i++) {
+			vertex.add(i);
+		}
+
 		Iterator<Boolean> iterator = ll.iterator();
 		for (i = 0; i < size; i++)
 			for (int j = 0; j < size; j++)
-				this.edge[i][j] = iterator.next();
+				if (iterator.next()) edges.add(new Pair<Integer, Integer>(i, j));
 	}
 
-	public Graph(int size) {
-		this.size = size;
-		edge = new boolean[size][size];
+	public Graph(HashSet<Integer> vertex, HashSet<Pair<Integer, Integer>> edges) {
+		this.vertex = vertex;
+		this.edges = edges;
 	}
 
-	public Graph(int size, boolean edge[][]) {
-		this.size = size;
-		edge = new boolean[size][size];
-		for (int i = 0; i < size; i++)
-			for (int j = 0; j < size; j++)
-				this.edge[i][j] = edge[i][j];
+	public Graph(Graph graph) {
+		vertex = new HashSet<Integer>();
+		edges = new HashSet<Pair<Integer, Integer>>();
+		for (Integer i : graph.vertex) vertex.add(i);
+		for (Pair<Integer, Integer> p : graph.edges) edges.add(new Pair<Integer, Integer>(p.getKey(), p.getValue()));
 	}
 
 	public Graph subGraph() {
+		Proof.scanner.nextLine();
 		HashSet<Integer> remainingSet = new HashSet<Integer>();
-		for (String num : scanner.nextLine().split(" "))
+		for (String num : Proof.scanner.nextLine().split(" "))
 			remainingSet.add(Integer.parseInt(num));
 		return subGraph(remainingSet);
 	}
 
 	public Graph subGraph(HashSet<Integer> remainingSet) {
-		int newSize = remainingSet.size();
-		Graph ret = new Graph(newSize);
-		LinkedList<Boolean> ll = new LinkedList<Boolean>();
-		for (int i = 0; i < size; i++)
-			if (remainingSet.contains(i))
-				for (int j = 0; j < size; j++)
-					if (remainingSet.contains(i))
-						ll.add(edge[i][j]);
-
-		Iterator<Boolean> iterator = ll.iterator();
-		for (int i = 0; i < newSize; i++)
-			for (int j = 0; j < newSize; j++)
-				ret.edge[i][j] = iterator.next();
-		return ret;
+		Graph newGraph = new Graph(this);
+		Iterator<Integer> iterator = newGraph.vertex.iterator();
+		while (iterator.hasNext())
+			if (!remainingSet.contains(iterator.next()))
+				iterator.remove();
+		Iterator<Pair<Integer, Integer>> iterator2 = newGraph.edges.iterator();
+		while (iterator2.hasNext()) {
+			Pair<Integer, Integer> next = iterator2.next();
+			if ((!remainingSet.contains(next.getKey())) || (!remainingSet.contains(next.getValue())))
+				iterator2.remove();
+		}
+		return newGraph;
 	}
 
+	public Graph isomorphism() {
+		Proof.scanner.nextLine();
+		HashMap<Integer, Integer> alpha = new HashMap<Integer, Integer>();
+		for (String item : Proof.scanner.nextLine().split(" ")) {
+			int comma = item.indexOf(',');
+			alpha.put(
+					Integer.parseInt(item.substring(0, comma)),
+					Integer.parseInt(item.substring(comma + 1, item.length())));
+		}
+		return isomorphism(alpha);
+	}
 
+	public Graph isomorphism(HashMap<Integer, Integer> alpha) {
+		HashSet<Integer> vertex = new HashSet<Integer>();
+
+		for (Integer i : this.vertex)
+			if (alpha.containsKey(i))
+				vertex.add(alpha.get(i));
+			else
+				vertex.add(i);
+
+		HashSet<Pair<Integer, Integer>> edges = new HashSet<Pair<Integer, Integer>>();
+		int first, last;
+		for (Pair<Integer, Integer> e : this.edges) {
+			if (alpha.containsKey(e.getKey()))
+				first = alpha.get(e.getKey());
+			else
+				first = e.getKey();
+			if (alpha.containsKey(e.getValue()))
+				last = alpha.get(e.getValue());
+			else
+				last = e.getValue();
+			edges.add(new Pair<Integer, Integer>(first, last));
+		}
+
+		return new Graph(vertex, edges);
+	}
 }
--- src/Proof.java
+++ src/Proof.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Proof {
+	public static Scanner scanner;
 	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		Graph g = new Graph(scanner);
+		scanner = new Scanner(System.in);
+		Graph g = new Graph();
 		Graph subg = g.subGraph();
+		Graph iso = subg.isomorphism();
 		System.out.println();
 	}
 }
